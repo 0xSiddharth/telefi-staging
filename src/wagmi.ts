dfdd9b5edef6febb5b8488d80ee65d5358f8937c
Siddharth <siddharthgk@yahoo.com>
2023-02-28 01:45:44 -0700

    Initial commit from create-wagmi

---
+++ src/wagmi.ts
@@ -0,0 +1,19 @@
+import { modalConnectors, walletConnectProvider } from '@web3modal/ethereum'
+import { configureChains, createClient } from 'wagmi'
+import { goerli, mainnet } from 'wagmi/chains'
+
+export const walletConnectProjectId = 'ba7804e457fbb5f1375cbdc14e679617'
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [mainnet, ...(import.meta.env?.MODE === 'development' ? [goerli] : [])],
+  [walletConnectProvider({ projectId: walletConnectProjectId })],
+)
+
+export const client = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({ appName: 'My wagmi + Web3Modal App', chains }),
+  provider,
+  webSocketProvider,
+})
+
+export { chains }
