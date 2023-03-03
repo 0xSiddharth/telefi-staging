00586c45eab2b71ec38d896dd85efa7c93c028a0
jaimindp <jaiminpa@usc.edu>
2023-03-02 19:06:33 -0700

    deploying with wallet add

--- src/App.tsx
+++ src/App.tsx
@@ -23,17 +23,12 @@ export function App() {
       </div>
     )
   }
-
-  function handleClick(){
-    Telegram.sendData(JSON.stringify({"hi":"0xe83fa30A48CC2E00BD1F7b6a9a6C34741F1dF688"}))
-    console.log('Button was clicked!');
-  }
  
   return <div>{/* Connect wallet content goes here */}
   <Helmet>
     <script src="https://telegram.org/js/telegram-web-app.js" type="text/javascript"/>
   </Helmet>
-  {/* <button onClick={handleClick}>Send this address 0xe83fa30A48CC2E00BD1F7b6a9a6C34741F1dF688 Back to TG</button> */}
+  
   <h1>wagmi + Web3Modal + Vite</h1>
 
   <Web3Button />
--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -8,19 +8,30 @@ export function Account() {
   const { data: ensName } = useEnsName({ address })
   const Telegram = window.Telegram.WebApp;
   
-  useEffect(() => {
-      if (address !== undefined){
-          Telegram.sendData(JSON.stringify({"hi":address}))
-          console.log('Send Addy')
-      }
-  }, [address]);
+  // useEffect(() => {
+  //     if (address !== undefined){
+  //         console.log('Send Addy')
+  //     }
+  // }, [address]);
 
-
+  function handleClick(){
+    if (address !== undefined){
+      console.log('Send Addy')
+      Telegram.sendData(JSON.stringify({"hi":address}))
+      console.log('Button was clicked!');
+    }
+  }
 
   return (
     <div>
-      {ensName ?? address}
-      {ensName ? ` (${address})` : null}
+      <div>
+        {ensName ?? address}
+        {ensName ? ` (${address})` : null}
+      </div>
+      <div>
+          <button onClick={handleClick}>Share ${address} wallet to receive payments</button>
+
+      </div>
     </div>
   )
 }
